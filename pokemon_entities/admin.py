f2f49712438703226da341a1d8e03d2c5b1a80da
KirillYabl <kirill-yablunovskii@mail.ru>
2020-06-24 20:00:53 +0300

    1. add inline element_type for pokemon

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Pokemon, PokemonEntity, PokemonElementType
 
-admin.site.register(Pokemon)
+
+class ElementTypeInline(admin.TabularInline):
+    model = Pokemon.element_type.through
+    extra = 0
+
+
+class PokemonAdmin(admin.ModelAdmin):
+    inlines = [
+        ElementTypeInline,
+    ]
+    exclude = ('element_type',)
+
+
+admin.site.register(Pokemon, PokemonAdmin)
 admin.site.register(PokemonEntity)
 admin.site.register(PokemonElementType)
